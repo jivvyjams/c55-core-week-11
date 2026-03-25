89d29db4f1db99738c7f43649c653da29d4dfe3f
Jawad Al Bdiwi <github.2snsr@passmail.com>
2026-03-25 18:46:13 +0100

    assignment task 1 complete

--- task-1/Time.js
+++ task-1/Time.js
@@ -1,3 +1,47 @@
 export class Time {
-  // Your code here
+  #secondsFromMidnight;
+  constructor(hours, minutes, seconds) {
+    this.#secondsFromMidnight = hours * 3600 + minutes * 60 + seconds;
+    this.#normalize();
+  }
+
+  #normalize() {
+    this.#secondsFromMidnight %= 86400;
+    if (this.#secondsFromMidnight < 0) {
+      this.#secondsFromMidnight += 86400;
+    }
+  }
+
+  getSeconds() {
+    return this.#secondsFromMidnight % 60;
+  }
+
+  getMinutes() {
+    return Math.floor((this.#secondsFromMidnight % 3600) / 60);
+  }
+
+  getHours() {
+    return Math.floor(this.#secondsFromMidnight / 3600);
+  }
+
+  addSeconds(seconds) {
+    this.#secondsFromMidnight += seconds;
+    this.#normalize();
+  }
+
+  addMinutes(minutes) {
+    this.addSeconds(minutes * 60);
+  }
+
+  addHours(hours) {
+    this.addSeconds(hours * 3600);
+  }
+
+  toString() {
+    const hh = String(this.getHours()).padStart(2, "0");
+    const mm = String(this.getMinutes()).padStart(2, "0");
+    const ss = String(this.getSeconds()).padStart(2, "0");
+
+    return `${hh}:${mm}:${ss}`;
+  }
 }
